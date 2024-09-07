32954ce4f8d804d7cca95d2966835c4785fd16cf
Ole-Kenneth Bratholt <ok@rait.no>
2024-09-07 20:42:17 +0200

    Fetch in background every 60s
    - Click on list item to refresh
    - Update on interval every 60s
    - Disable timer onWillEnterBackground

--- stengttunnel/Carplay/CarPlaySceneDelegate.swift
+++ stengttunnel/Carplay/CarPlaySceneDelegate.swift
@@ -8,67 +8,86 @@
 import Foundation
 import CarPlay
 
+struct ListItem {
+    let favorite: Favorite
+    let item: CPListItem
+}
+
 class CarPlaySceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate, UISceneDelegate {
     private var store = FavoriteStore()
     private var storeManager = StoreManager.shared
-
-    private var interfaceController: CPInterfaceController?
+    private var timer = Timer()
+    private var listItems = [ListItem]()
     
+    private var interfaceController: CPInterfaceController?
     
     func sceneDidBecomeActive(_ scene: UIScene) {
         print("sceneDidBecomeActive")
     }
-
+    
     func sceneWillResignActive(_ scene: UIScene) {
         print("sceneWillResignActive")
+        timer.invalidate()
     }
-
     
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        print("sceneWillEnterForeground")
+    func drawScreen() {
+        timer.invalidate()
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 60, repeats: true, block: { _ in
+            print("Updating list")
+            self.listItems.forEach { listItem in
+                self.updateListItem(item: listItem.item, favorite: listItem.favorite) {
+                    // All good
+                }
+            }
+        })
 
+        
         let favoriteList = CPListTemplate(title: "Stengt tunnel", sections: [
-            CPListSection(items: [
-            ])
+            CPListSection(items: [])
         ])
-
-        loadStore {
-            favoriteList.updateSections([
+            
+        var sections = [CPListSection]()
+        let items = self.listItems.map({ $0.item })
+        
+        if self.storeManager.subscriptionActive {
+            sections = [
+                CPListSection(items:items)
+            ]
+        } else {
+            sections = [
+                CPListSection(items: Array(items.prefix(2))),
                 
-                CPListSection(items: self.store.favorites.prefix(2).map({ favorite in
-                    self.fetchRoad(favorite)
-                })),
-                
-                CPListSection(items: self.store.favorites.suffix(from: 2).map({ favorite in
-                    self.getListItemFromRoad(favorite: favorite, status: Status(statusMessage: "", status: .yellow))
-                }), header: String(localized: "Buy Stengt tunnel+"), sectionIndexTitle: "")
-            ])
+                CPListSection(items: Array(items.suffix(from: 2)), header: String(localized: "Buy Stengt tunnel+"), sectionIndexTitle: "")
+            ]
         }
+        favoriteList.updateSections(sections)
         
         favoriteList.tabSystemItem = .favorites
 
-        // let closestList = CPListTemplate(title: "Nærmeste", sections: [
-        //     CPListSection(items: [
-        //         CPListItem(text: "Operatunnelen", detailText: "ser ut til å være åpen", image: TrafficLightView(color: .green).render()),
-        //         CPListItem(text: "Vaterlandstunnelen", detailText: "ser ut til å være stengt", image: TrafficLightView(color: .red).render()),
-        //         CPListItem(text: "Oslofjordtunnelen", detailText: "ser ut til å være åpen", image: TrafficLightView(color: .green).render()),
-        //     ])
-        // ])
-        //
-        // closestList.tabImage = UIImage(systemName: "map.fill")
-
-        
-        // let tabBar = CPTabBarTemplate(templates: [favoriteList, closestList])
-        interfaceController?.setRootTemplate(favoriteList, animated: true, completion: { _,_ in
+        interfaceController?.setRootTemplate(favoriteList, animated: true, completion: { _, _ in
             // Do nothing
         })
-
     }
-
+    
+    
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        print("sceneWillEnterForeground")
+        loadStore {
+            self.listItems = []
+            
+            self.store.favorites.forEach({ favorite in
+                self.listItems.append(ListItem(favorite: favorite, item: self.fetchRoad(favorite)))
+            })
+            
+            self.drawScreen()
+        }
+    }
+    
     func sceneDidEnterBackground(_ scene: UIScene) {
         print("sceneDidEnterBackground")
     }
-
+    
     
     func loadStore(_ completion: @escaping () -> Void) {
         Task {
@@ -81,35 +100,46 @@ class CarPlaySceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate, UIS
         }
     }
     
-    func getListItemFromRoad(favorite: Favorite, status: Status) -> CPListItem {
-        let item = CPListItem(text: favorite.roadName, detailText: String(localized: "is ..."), image: TrafficLightView(color: .yellow).render(blurred: true))
+    func getListItemFromFavorite(favorite: Favorite, blurred: Bool = true) -> CPListItem {
+        let item = CPListItem(text: favorite.roadName, detailText: String(localized: "is ..."), image: TrafficLightView(color: .yellow).render(blurred: blurred))
         item.handler = { item, completion in
-            print("Clicked \(favorite.roadName)")
+            print("Disabled click on \(favorite.roadName)")
             
             completion()
         }
-
+        
         return item
     }
-
-    func fetchRoad(_ favorite: Favorite) -> CPListItem {
-        let item = CPListItem(text: favorite.urlFriendly.localizedCapitalized, detailText: String(localized: "is ..."), image: TrafficLightView(color: .yellow).render())
-        item.handler = { item, completion in
-            print("Clicked \(favorite.roadName)")
-            
-            completion()
-        }
-
+    
+    func updateListItem(item: CPListItem, favorite: Favorite, callback: @escaping () -> Void) {
         Dataloader.shared.loadRoad(road: favorite.urlFriendly) { status in
             if let status {
                 item.setText(favorite.roadName)
                 item.setDetailText(status.statusMessage.replacingOccurrences(of: favorite.roadName + " ", with: ""))
                 item.setImage(TrafficLightView(color: status.status).render())
             }
+
+            callback()
         }
+    }
+    
+    func fetchRoad(_ favorite: Favorite) -> CPListItem {
+        let item = getListItemFromFavorite(favorite: favorite, blurred: false)
+        item.handler = { item, completion in
+            print("Clicked \(favorite.roadName)")
+            self.updateListItem(item: item as! CPListItem, favorite: favorite) {
+                do {usleep(500000)}
+                completion()
+            }
+        }
+        
+        updateListItem(item: item, favorite: favorite) {
+            // Do nothing
+        }
+        
         return item
     }
-        
+    
     func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene,
                                   didConnect interfaceController: CPInterfaceController) {
         self.interfaceController = interfaceController
